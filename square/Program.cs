602e5649e78def8372b94b47698eb4e34409e8ac
deriansalazar <derian99.ds@gmail.com>
2024-10-17 12:32:40 +0200

    kvadratens modell

--- square/Program.cs
+++ square/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+namespace SquareApi.Models
+{
+    public class Square
+    {
+
+        public int Id { get; set; }
+        public double SideLength { get; set; }
+        public double Area
+        {
+            get { return SideLength * SideLength; }
+        }
+    }
+}
--- square/obj/Debug/net8.0/square.AssemblyInfo.cs
+++ square/obj/Debug/net8.0/square.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("square")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb03f9d59c8a660a820a725fade6bc880dd65ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187e60d6b3b6b5ae8c3718e81c5fbdd8b9cb1626")]
 [assembly: System.Reflection.AssemblyProductAttribute("square")]
 [assembly: System.Reflection.AssemblyTitleAttribute("square")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
