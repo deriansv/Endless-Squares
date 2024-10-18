22433f81d349f0588e46b0d266747aacb3823b7c
deriansalazar <derian99.ds@gmail.com>
2024-10-18 06:41:41 +0200

    - centrerad button med button container och position absolute - startade även squareApi servern

--- SquareApi/obj/Debug/net8.0/SquareApi.AssemblyInfo.cs
+++ SquareApi/obj/Debug/net8.0/SquareApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SquareApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602e5649e78def8372b94b47698eb4e34409e8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceee9eee32eb827bd378790761a699405e389e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("SquareApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SquareApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- square/obj/Debug/net8.0/square.AssemblyInfo.cs
+++ square/obj/Debug/net8.0/square.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("square")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187e60d6b3b6b5ae8c3718e81c5fbdd8b9cb1626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceee9eee32eb827bd378790761a699405e389e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("square")]
 [assembly: System.Reflection.AssemblyTitleAttribute("square")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
